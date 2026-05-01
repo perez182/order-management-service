800c3bdc6722261d61f7e93e962025bc960e98bd
unknown <pbautistadavid@gmail.com>
2026-04-30 20:13:53 -0600

    feat: add order client

--- src/main/java/com/nova/order/management/service/infrastructure/client/OrderClient.java
+++ src/main/java/com/nova/order/management/service/infrastructure/client/OrderClient.java
@@ -1,5 +1,24 @@
 package com.nova.order.management.service.infrastructure.client;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.nova.order.management.service.api.dto.request.OrderRequest;
+import com.nova.order.management.service.api.dto.response.OrderResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
 public class OrderClient {
 
+    private final RestTemplate restTemplate;
+
+    @Value("${services.order.url}")
+    private String orderUrl;
+
+    public OrderResponse create(OrderRequest request) {
+        return restTemplate.postForObject(orderUrl, request, OrderResponse.class);
+    }
 }
